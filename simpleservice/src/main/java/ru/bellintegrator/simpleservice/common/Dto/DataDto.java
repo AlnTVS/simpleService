9588f6cf8708140ea4dfff98d71701c177a21df1
AlnTVS <alntvs@yandex.ru>
2022-01-21 00:10:40 +0300

    Add RestControllerAdvise and DataDto

---
+++ simpleservice/src/main/java/ru/bellintegrator/simpleservice/common/Dto/DataDto.java
@@ -0,0 +1,10 @@
+package ru.bellintegrator.simpleservice.common.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class DataDto {
+
+    private Object data;
+}
---
+++ simpleservice/src/main/java/ru/bellintegrator/simpleservice/common/controller/CommonController.java
@@ -0,0 +1,25 @@
+package ru.bellintegrator.simpleservice.common.controller;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import ru.bellintegrator.simpleservice.common.Dto.DataDto;
+
+@RestControllerAdvice
+public class CommonController implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        DataDto data = new DataDto();
+        data.setData(body);
+        return data;
+    }
+}
