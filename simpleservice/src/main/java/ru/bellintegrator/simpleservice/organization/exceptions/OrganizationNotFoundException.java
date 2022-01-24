200977d4ea29264b671c50b374b8de4fef512ae5
AlnTVS <alntvs@yandex.ru>
2022-01-24 21:11:23 +0300

    JavaDoc: add OrganizationNotFoundException.java

--- simpleservice/src/main/java/ru/bellintegrator/simpleservice/organization/exceptions/OrganizationNotFoundException.java
+++ simpleservice/src/main/java/ru/bellintegrator/simpleservice/organization/exceptions/OrganizationNotFoundException.java
@@ -2,6 +2,14 @@ package ru.bellintegrator.simpleservice.organization.exceptions;
 
 import ru.bellintegrator.simpleservice.common.exception.SimpleServiceException;
 
+/**
+ * Расширяет класс {@link SimpleServiceException}. Используется для генерации ошибок связанных с бизнес-логикой сервиса в случае,
+ * когда организация с указанным id не была найдена в БД.
+ *
+ * @author Alntvs alntvs@yandex.ru https://github.com/AlnTVS
+ * @version 1.0
+ * @since 21.01.2021
+ */
 public class OrganizationNotFoundException extends SimpleServiceException {
     public OrganizationNotFoundException(String message) {
         super(message);
